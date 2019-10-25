371b97bb5815e8ba070b9c90d941a36a8f7eb1ff
artsok <relix@mail.ru>
2019-10-25 03:31:05 +0300

    added main page

---
+++ lib/widget/page/charts/main_chars.dart
@@ -0,0 +1,592 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_sparkline/flutter_sparkline.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:flutter_circular_chart/flutter_circular_chart.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:wifi_scanner/model/device_info.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  SharedPreferences initShareStatePrefs;
+  DeviceInfo _deviceInfo;
+
+  var data = [0.0, 1.0, 3.0, 4.0, 7.0, 8.0, 9.0, 5.0, 10.0, 5.0, 12.0];
+  var data1 = [0.0, -2.0, 3.5, -2.0, 0.5, 0.7, 0.8, 1.0, 2.0, 3.0, 3.2];
+
+  List<CircularStackEntry> circularData = <CircularStackEntry>[
+    new CircularStackEntry(
+      <CircularSegmentEntry>[
+        new CircularSegmentEntry(18.0, Color(0xffec3337), rankKey: 'BAD'),
+        new CircularSegmentEntry(10.0, Color(0xff40b24b), rankKey: 'GOOD'),
+      ],
+      rankKey: 'Quarterly Profits',
+    ),
+  ];
+
+  Material myTextItems(String title, String subtitle) {
+    return Material(
+      color: Colors.white,
+      elevation: 14.0,
+      borderRadius: BorderRadius.circular(24.0),
+      shadowColor: Color(0x802196F3),
+      child: Center(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.blueAccent,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      subtitle,
+                      style: TextStyle(
+                        fontSize: 30.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Material myCircularItems(String title, String subtitle) {
+    return Material(
+      color: Colors.white,
+      elevation: 14.0,
+      borderRadius: BorderRadius.circular(24.0),
+      shadowColor: Color(0x802196F3),
+
+      child: Center(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.blueAccent,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: AnimatedCircularChart(
+                      size: const Size(100.0, 100.0),
+                      initialChartData: circularData,
+                      chartType: CircularChartType.Pie,
+                    ),
+                  ),
+
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      "Доступно - 10/Не доступно - 20",
+                      style: TextStyle(
+                        fontSize: 15.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Material mychart1Items(String title, String priceVal, String subtitle) {
+    return Material(
+      color: Colors.white,
+      elevation: 14.0,
+      borderRadius: BorderRadius.circular(24.0),
+      shadowColor: Color(0x802196F3),
+      child: Center(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.blueAccent,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: Text(
+                      priceVal,
+                      style: TextStyle(
+                        fontSize: 30.0,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: Text(
+                      subtitle,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: new Sparkline(
+                      data: data,
+                      lineColor: Color(0xffff6101),
+                      pointsMode: PointsMode.all,
+                      pointSize: 8.0,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Material mychartDeviceInfo(String title) {
+    return Material(
+      color: Colors.white,
+      elevation: 14.0,
+      borderRadius: BorderRadius.circular(24.0),
+      shadowColor: Color(0x802196F3),
+      child: Column(
+        children: <Widget>[
+          Center(
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.all(.0),
+                        child: Text(
+                          title,
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.blueAccent,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 0.0, horizontal: 15.0),
+                      child: Text(
+                        "Модель устройства:",
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.blueGrey,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 0.0, horizontal: 15.0),
+                      child: Text(
+                        "Платформа:",
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.blueGrey,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 0.0, horizontal: 15.0),
+                      child: Text(
+                        "ID Устройства:",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.blueGrey,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 0.0, horizontal: 15.0),
+                      child: Text(
+                        '${(Platform.isAndroid) ? _deviceInfo.androidDeviceInfo.model : _deviceInfo.iosDeviceInfo.model}',
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 0.0, horizontal: 15.0),
+                      child: Text(
+                        '${(Platform.isAndroid) ? _deviceInfo.platformVersion : _deviceInfo.iosDeviceInfo.systemVersion}',
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 0.0, horizontal: 15.0),
+                      child: Text(
+                        '${(Platform.isAndroid) ? _deviceInfo.androidDeviceInfo.androidId : _deviceInfo.iosDeviceInfo.identifierForVendor}',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Material mychart2Items(String title) {
+    return Material(
+      color: Colors.white,
+      elevation: 14.0,
+      borderRadius: BorderRadius.circular(24.0),
+      shadowColor: Color(0x802196F3),
+      child: Center(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(.0),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.blueAccent,
+                      ),
+                    ),
+                  ),
+
+//                  Padding(
+//                    padding: EdgeInsets.all(1.0),
+//                    child: Text("sda", style: TextStyle(
+//                      fontSize: 30.0,
+//                    ),),
+//                  ),
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: Text(
+                      "sadasd",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: Text(
+                      "sadasd",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: Text(
+                      "sadasd",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                  ),
+
+//                  Padding(
+//                    padding: EdgeInsets.all(1.0),
+//                    child: new Sparkline(
+//                      data: data1,
+//                      fillMode: FillMode.below,
+//                      fillGradient: new LinearGradient(
+//                        begin: Alignment.topCenter,
+//                        end: Alignment.bottomCenter,
+//                        colors: [Colors.amber[800], Colors.amber[200]],
+//                      ),
+//                    ),
+//                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+
+  Material editParams(String title) {
+    return Material(
+      color: Colors.white,
+      elevation: 14.0,
+      borderRadius: BorderRadius.circular(24.0),
+      shadowColor: Color(0x802196F3),
+      child: Center(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(.0),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.blueAccent,
+                      ),
+                    ),
+                  ),
+
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: Text("sda", style: TextStyle(
+                      fontSize: 30.0,
+                    ),),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: Text(
+                      "sadasd",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: Text(
+                      "sadasd",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: Text(
+                      "sadasd",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                  ),
+
+//                  Padding(
+//                    padding: EdgeInsets.all(1.0),
+//                    child: new Sparkline(
+//                      data: data1,
+//                      fillMode: FillMode.below,
+//                      fillGradient: new LinearGradient(
+//                        begin: Alignment.topCenter,
+//                        end: Alignment.bottomCenter,
+//                        colors: [Colors.amber[800], Colors.amber[200]],
+//                      ),
+//                    ),
+//                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {
+              //
+            }),
+        title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(FontAwesomeIcons.chartLine),
+              onPressed: () {
+                //
+              }),
+        ],
+      ),
+      body: Container(
+        color: Color(0xffE5E5E5),
+        child: StaggeredGridView.count(
+          crossAxisCount: 4,
+          crossAxisSpacing: 12.0,
+          mainAxisSpacing: 12.0,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: mychart1Items(
+                  "Количество подключенных устройств в ВСП № ${json.decode(initShareStatePrefs.getString("user"))["branch"]}\n",
+                  "12",
+                  ""),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: myCircularItems("Доступность ресурсов", ""),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: myTextItems("Ср.входящее соединение", "1.61 МБайт/с"),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: myTextItems("Ср.исходящее соединение", "868.28 КБайт/с"),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: mychartDeviceInfo("Информация об устройстве"),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: editParams("Редактирование настроек"),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: mychart2Items("Инструментарий"),
+            ),
+          ],
+          staggeredTiles: [
+            StaggeredTile.extent(4, 250.0),
+            StaggeredTile.extent(2, 250.0),
+            StaggeredTile.extent(2, 120.0),
+            StaggeredTile.extent(2, 120.0),
+            StaggeredTile.extent(4, 250.0),
+            StaggeredTile.extent(2, 250.0),
+            StaggeredTile.extent(2, 250.0),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    initStatePrefs();
+    _getDeviceInfo();
+    super.initState();
+  }
+
+  initStatePrefs() async {
+    var sharedPreferences = await SharedPreferences.getInstance();
+    setState(() {
+      initShareStatePrefs = sharedPreferences;
+    });
+  }
+
+  _getDeviceInfo() async {
+    DeviceInfo deviceInfo = await DeviceInfo.instance;
+    setState(() {
+      _deviceInfo = deviceInfo;
+    });
+  }
+}
